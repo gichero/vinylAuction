eff3ce38d9b531170b3780eb0bb192fae20307e0
Amos <amosgathungu@gmail.com>
2017-05-18 22:18:59 -0400

    render message

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import * as Redux from 'redux';
+import * as ReactRedux from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import {Router, Route, Link, IndexLink, IndexRoute, hashHistory} from 'react-router';
 import './index.css';
 
+
+
+
+class AppLayout extends React.Component {
+    render(){
+
+        return(
+        <div>
+            <h1>Welcome to the Vinyl Auction</h1>
+        </div>
+    );
+    }
+}
+
 ReactDOM.render(
-  <App />,
+    <Router history={hashHistory}>
+        <Route path = "/"
+            component={AppLayout}>
+        </Route>
+    </Router>,
   document.getElementById('root')
 );
